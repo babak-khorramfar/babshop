f754b522c9606561e0985fc384bc35c0240e9702
Babak Khorramfar <info.webschoool@gmail.com>
2025-02-12 01:08:27 +0330

    Create Database

--- babshop/settings.py
+++ babshop/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'babshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'babshop',
+        'USER': 'postgres',
+        'PASSWORD': '702400',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
